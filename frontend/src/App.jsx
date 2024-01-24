1c2dd40942aa6f607adbd3f931a33dc71c7411ea
NumanBashir <sportybashir@gmail.com>
2024-01-24 21:04:05 +0100

    added date fns to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Clock from "./components/Clock";
+import { format } from "date-fns";
 
 const App = () => {
   const [prayerTimes, setPrayerTimes] = useState({
